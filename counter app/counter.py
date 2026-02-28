6da376af85690fa88f4d7e4f5711174d7564594a
jdias2019 <joeldias1907@gmail.com>
2026-02-28 18:10:33 +0000

    better gui - counter app

--- counter app/counter.py
+++ counter app/counter.py
@@ -1,41 +1,45 @@
-import customtkinter 
+import customtkinter
 
 
-customtkinter.set_appearance_mode("dark")  
-customtkinter.set_default_color_theme("green")  
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
 
-gui = customtkinter.CTk()  
-gui.title("Counter App")  
+gui = customtkinter.CTk()
+gui.title("Counter App")
 gui.geometry("1000x600")
 
 
 
 count = 0
 counter_label = customtkinter.CTkLabel(gui, text=str(count), font=customtkinter.CTkFont(size=90))
-counter_label.pack(pady=50)
+counter_label.pack(pady=80)
 
 def plus():
     global count
     count = count + 1
     counter_label.configure(text=str(count))
 
-plus_button = customtkinter.CTkButton(gui, text="+", command=plus)
-plus_button.pack(pady=50)
-
 def minus():
     global count
     count = count - 1
     counter_label.configure(text=str(count))
-
-minus_button = customtkinter.CTkButton(gui, text="-", command=minus)
-minus_button.pack(pady=50)
 
 def reset():
     global count
     count = 0
     counter_label.configure(text=str(count))
 
-reset_button = customtkinter.CTkButton(gui, text="Reset", command=reset)
-reset_button.pack(pady=50)
+
+frame_butons = customtkinter.CTkFrame(gui, fg_color="transparent")
+frame_butons.pack(pady=50)
+
+plus_button  = customtkinter.CTkButton(frame_butons, text="+",     command=plus,  width=150, height=50, font=customtkinter.CTkFont(size=24))
+minus_button = customtkinter.CTkButton(frame_butons, text="-",     command=minus, width=150, height=50, font=customtkinter.CTkFont(size=24))
+reset_button = customtkinter.CTkButton(frame_butons, text="Reset", command=reset, width=150, height=50)
+
+plus_button.pack(side="left",  padx=40)
+minus_button.pack(side="left", padx=40)
+reset_button.pack(side="left", padx=40)
+
 
 gui.mainloop()
